ee4e7a3775b18c5a260a0725311972301f915ed0
Sunghun <dev.sunghun@gmail.com>
2025-01-28 14:12:11 +0900

    test: 테스트 케이스 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,6 +2,9 @@ package calculator;
 
 public class StringCalculator {
   public int calculateSum(Input input) {
+    if(input == null) {
+      return 0;
+    }
     return input.getPositiveNumbers().stream().mapToInt(PositiveNumber::getValue).sum();
   }
 }
--- src/test/java/calculator/PositiveNumberTest.java
+++ src/test/java/calculator/PositiveNumberTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class PositiveNumberTest {
+
+  @DisplayName("문자열을 숫자 값으로 가져올 수 있다.")
+  @CsvSource({"'100', 100", "'0', 0", "'1', 1"})
+  @ParameterizedTest
+  void getValue(String inputValue, int expectedValue) {
+    // when
+    PositiveNumber positiveNumber = new PositiveNumber(inputValue);
+
+    // then
+    assertThat(positiveNumber.getValue()).isEqualTo(expectedValue);
+  }
 
   @DisplayName("문자열이 숫자가 아니면 예외를 발생시킨다.")
   @CsvSource({"삼", "일", "이"})
@@ -18,7 +30,7 @@ class PositiveNumberTest {
         .hasMessage(ErrorMessage.NOT_NUMBER);
   }
 
-  @DisplayName("split된 문자열이 음수면 예외를 발생시킨다.")
+  @DisplayName("문자열이 음수면 예외를 발생시킨다.")
   @CsvSource({"-1", "-10"})
   @ParameterizedTest
   void parseStringToNumberThrowNegativeNumber(String inputValue) {
