1359a49551d801dd2e9d573e6d216f290fe7e7b8
dev-sunghun <dev.sunghun@gmail.com>
2025-02-04 15:26:56 +0900

    test: OrderServiceTest 테이스 작업

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -6,9 +6,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.OrderType;
 import kitchenpos.mock.TestContainer;
@@ -31,10 +34,11 @@ class OrderServiceTest {
 
     private OrderService orderService;
     private TestContainer testContainer;
+    private FakeOrderRepository orderRepository;
 
     @BeforeEach
     void setUp() {
-        FakeOrderRepository orderRepository = new FakeOrderRepository();
+        orderRepository = new FakeOrderRepository();
         FakeMenuRepository menuRepository = new FakeMenuRepository();
         FakeOrderTableRepository orderTableRepository = new FakeOrderTableRepository();
         FakeKitchenridersClient kitchenridersClient = new FakeKitchenridersClient();
@@ -217,6 +221,224 @@ class OrderServiceTest {
         assertThat(savedOrder.getOrderLineItems().size()).isEqualTo(
             order.getOrderLineItems().size());
     }
+
+    @DisplayName("주문을 수락할 수 있다.")
+    @Test
+    void accept() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+
+        // when
+        Order updatedOrder = orderService.accept(orderId);
+
+        // then
+        assertThat(updatedOrder.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+    }
+
+    @DisplayName("존재하지 않는 주문을 수락 시, 예외가 발생 한다.")
+    @Test
+    void acceptNoSuchElementException() {
+        // given when then
+        assertThatThrownBy(() -> orderService.accept(UUID.randomUUID()))
+            .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @DisplayName("대기 중 상태가 아닌 주문을 수락 시, 예외가 발생 한다.")
+    @Test
+    void acceptStatusException() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        savedOrder.setStatus(OrderStatus.COMPLETED);
+        Order updatedOrder = orderRepository.save(savedOrder);
+        UUID orderId = updatedOrder.getId();
+
+        // when then
+        assertThatThrownBy(() -> orderService.accept(orderId))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("주문을 제공됨 처리할 수 있다.")
+    @Test
+    void serve() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+        orderService.accept(orderId);
+
+        // when
+        Order updatedOrder = orderService.serve(orderId);
+
+        // then
+        assertThat(updatedOrder.getStatus()).isEqualTo(OrderStatus.SERVED);
+    }
+
+    @DisplayName("수락됨 상태가 아닌 주문을 제공 시, 예외가 발생 한다.")
+    @Test
+    void serveStatusException() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+
+        // when then
+        assertThatThrownBy(() -> orderService.serve(orderId))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("주문을 배달시작됨 처리할 수 있다.")
+    @Test
+    void startDelivery() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+        orderService.accept(orderId);
+        orderService.serve(orderId);
+
+        // when
+        Order updatedOrder = orderService.startDelivery(orderId);
+
+        // then
+        assertThat(updatedOrder.getStatus()).isEqualTo(OrderStatus.DELIVERING);
+    }
+
+    @DisplayName("배달 주문이 아닌데 배달시작됨 처리 시, 예외가 발생 한다.")
+    @Test
+    void startDeliveryTypeException() {
+        // given
+        Order savedOrder = orderService.create(createEatInOrder());
+        UUID orderId = savedOrder.getId();
+        orderService.accept(orderId);
+        orderService.serve(orderId);
+
+        // when then
+        assertThatThrownBy(() -> orderService.startDelivery(orderId))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("제공됨 상태가 아닌데 배달시작됨 처리 시, 예외가 발생 한다.")
+    @Test
+    void startDeliveryStatusException() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+
+        // when then
+        assertThatThrownBy(() -> orderService.startDelivery(orderId))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("주문을 배달완료됨 처리할 수 있다.")
+    @Test
+    void completeDelivery() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+        orderService.accept(orderId);
+        orderService.serve(orderId);
+        orderService.startDelivery(orderId);
+
+        // when
+        Order updatedOrder = orderService.completeDelivery(orderId);
+
+        // then
+        assertThat(updatedOrder.getStatus()).isEqualTo(OrderStatus.DELIVERED);
+    }
+
+    @DisplayName("배달시작됨 상태가 아닌데 배달완료됨 처리 시, 예외가 발생 한다.")
+    @Test
+    void completeDeliveryStatusException() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+
+        // when then
+        assertThatThrownBy(() -> orderService.startDelivery(orderId))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("배달 주문을 완료됨 처리할 수 있다.")
+    @Test
+    void completeDeliveryOrder() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+        orderService.accept(orderId);
+        orderService.serve(orderId);
+        orderService.startDelivery(orderId);
+        orderService.completeDelivery(orderId);
+
+        // when
+        Order updatedOrder = orderService.complete(orderId);
+
+        // then
+        assertThat(updatedOrder.getStatus()).isEqualTo(OrderStatus.COMPLETED);
+    }
+
+    @DisplayName("배달 주문의 경우 배달완료됨 상태가 아닌데 완료됨 처리 시, 예외가 발생 한다.")
+    @Test
+    void completeTypeDeliveryStatusException() {
+        // given
+        Order savedOrder = orderService.create(createDeliveryOrder());
+        UUID orderId = savedOrder.getId();
+        orderService.accept(orderId);
+        orderService.serve(orderId);
+        orderService.startDelivery(orderId);
+
+        // when then
+        assertThatThrownBy(() -> orderService.complete(orderId))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("테이크아웃 주문의 경우 제공됨 상태가 아닌데 완료됨 처리 시, 예외가 발생 한다.")
+    @Test
+    void completeTypeTakeoutStatusException() {
+        // given
+        Order savedOrder = orderService.create(createTakeoutOrder());
+        UUID orderId = savedOrder.getId();
+        orderService.accept(orderId);
+
+        // when then
+        assertThatThrownBy(() -> orderService.complete(orderId))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("매장 식사 주문을 완료됨 처리할 수 있다.")
+    @Test
+    void completeEatInOrder() {
+        // given
+        Order savedOrder = orderService.create(createEatInOrder());
+        UUID orderId = savedOrder.getId();
+        orderService.accept(orderId);
+        orderService.serve(orderId);
+
+        // when
+        Order updatedOrder = orderService.complete(orderId);
+
+        // then
+        assertThat(updatedOrder.getStatus()).isEqualTo(OrderStatus.COMPLETED);
+        assertThat(updatedOrder.getOrderTable().isOccupied()).isFalse();
+        assertThat(updatedOrder.getOrderTable().getNumberOfGuests()).isZero();
+    }
+
+    @DisplayName("저장된 주문 리스트를 조회할 수 있다.")
+    @Test
+    void findAll() {
+        // given
+        Order deliveryOrder = orderService.create(createDeliveryOrder());
+        Order eatInOrder = orderService.create(createEatInOrder());
+        Order takeoutOrder = orderService.create(createTakeoutOrder());
+
+        // when
+        List<Order> savedOrders = orderService.findAll();
+
+        // then
+        assertThat(savedOrders)
+            .hasSize(3)
+            .extracting(Order::getId)
+            .containsExactlyInAnyOrder(deliveryOrder.getId(),
+                eatInOrder.getId(), takeoutOrder.getId());
+    }
+
 
     private Order createDeliveryOrder() {
         String deliveryAddress = "서울시 주소";
